e718763ebd51ee6d7278eb1166d2672f309fdd61
Matias Saade <matiassaade@gmail.com>
2026-03-16 23:43:29 -0300

    fix: prisma ts

--- lib/db.ts
+++ lib/db.ts
@@ -14,11 +14,12 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
 const adapter = new PrismaNeon(pool as any);
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({ adapter });
+  globalForPrisma.prisma ?? new PrismaClient({ adapter, log: ["error", "query", "warn"] });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
