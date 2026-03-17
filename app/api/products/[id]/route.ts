6edbc6b6affcc592bc586ef6772bd2ab665a172a
Matias Saade <matiassaade@gmail.com>
2026-03-16 22:25:52 -0300

    fix: arreglado param id en api para editar y eliminar producto

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 
 export async function PUT(
     req: Request,
-    { params }: { params: { id: string } },
+    { params }: { params: Promise<{ id: string }> },
 ) {
     const { id } = await params;
     const body = await req.json();
@@ -17,7 +17,7 @@ export async function PUT(
 
 export async function DELETE(
     _: Request,
-    { params }: { params: { id: string } },
+    { params }: { params: Promise<{ id: string }> },
 ) {
     const { id } = await params;
     try {
